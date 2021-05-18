ae5a4c55f3de2c7eb1e75520de4b4d7bce67ef5f
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-18 18:55:29 -0300

    Customer find by id, exception handling

--- src/main/java/com/raulrezende/financialcontrol/services/CustomerService.java
+++ src/main/java/com/raulrezende/financialcontrol/services/CustomerService.java
@@ -1,6 +1,7 @@
 package com.raulrezende.financialcontrol.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.raulrezende.financialcontrol.entities.Customer;
 import com.raulrezende.financialcontrol.entities.dto.CustomerDTO;
 import com.raulrezende.financialcontrol.repositories.CustomerRepository;
+import com.raulrezende.financialcontrol.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CustomerService {
@@ -21,5 +23,12 @@ public class CustomerService {
 	public List<CustomerDTO> findAll() {
 		List<Customer> list = repository.findAll();
 		return list.stream().map(x -> new CustomerDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CustomerDTO findById(Long id) {
+		Optional<Customer> obj = repository.findById(id);
+		Customer entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new CustomerDTO(entity);
 	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_customer (firstName, lastName, email, password) VALUES ('LUCAS', 'RIBEIRO DUARTE', 'lucas@lucas.com.br', 'fV1%uUlI5');
-INSERT INTO tb_customer (firstName, lastName, email, password) VALUES ('PEDRO', 'NASCIMENTO SILVA', 'carlos@carlos.com.br', 'zV3%C20Pe');
-INSERT INTO tb_customer (firstName, lastName, email, password) VALUES ('JOAO', 'MARTINS COSTA', 'jose@jose.com.br', 'zK4@!3VWW');
-INSERT INTO tb_customer (firstName, lastName, email, password) VALUES ('CARLOS', 'TEIXEIRA FREITAS', 'jose@jose.com.br', 'rX2%&sx3*');
-INSERT INTO tb_customer (firstName, lastName, email, password) VALUES ('MARCOS', 'MEDEIROS GOMES', 'luiz@luiz.com.br', 'tU3%$8#7*');
+INSERT INTO tb_customer (firstName, lastName, email, password, premium) VALUES ('LUCAS', 'RIBEIRO DUARTE', 'lucas@lucas.com.br', 'fV1%uUlI5', false);
+INSERT INTO tb_customer (firstName, lastName, email, password, premium) VALUES ('PEDRO', 'NASCIMENTO SILVA', 'carlos@carlos.com.br', 'zV3%C20Pe', false);
+INSERT INTO tb_customer (firstName, lastName, email, password, premium) VALUES ('JOAO', 'MARTINS COSTA', 'jose@jose.com.br', 'zK4@!3VWW', false);
+INSERT INTO tb_customer (firstName, lastName, email, password, premium) VALUES ('CARLOS', 'TEIXEIRA FREITAS', 'jose@jose.com.br', 'rX2%&sx3*', false);
+INSERT INTO tb_customer (firstName, lastName, email, password, premium) VALUES ('MARCOS', 'MEDEIROS GOMES', 'luiz@luiz.com.br', 'tU3%$8#7*', false);
