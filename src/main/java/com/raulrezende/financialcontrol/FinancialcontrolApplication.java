a4e697f9f6f2c23f0575b2beeda2cfd1ccbeab57
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-18 17:09:51 -0300

    Project created

---
+++ src/main/java/com/raulrezende/financialcontrol/FinancialcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.raulrezende.financialcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raulrezende/financialcontrol/FinancialcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulrezende.financialcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
