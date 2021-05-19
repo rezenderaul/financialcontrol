2592e771dbfaa0b3be0ed56ba9959ccb46e44b64
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-18 21:48:10 -0300

    Update Customer

--- src/main/java/com/raulrezende/financialcontrol/services/CustomerService.java
+++ src/main/java/com/raulrezende/financialcontrol/services/CustomerService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,6 +40,19 @@ public class CustomerService {
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new CustomerDTO(entity);
+	}
+	
+	@Transactional
+	public CustomerDTO update(long id, CustomerDTO dto) {
+		try {
+			Customer entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new CustomerDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 	
 	private void copyDtoToEntity(CustomerDTO dto, Customer entity) {
