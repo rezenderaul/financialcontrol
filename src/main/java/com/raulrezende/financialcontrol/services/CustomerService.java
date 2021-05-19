e84808c37d66a9864f37431617f5cb809aebc6c9
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-18 22:01:14 -0300

    Delete Customer

--- src/main/java/com/raulrezende/financialcontrol/services/CustomerService.java
+++ src/main/java/com/raulrezende/financialcontrol/services/CustomerService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.raulrezende.financialcontrol.entities.Customer;
 import com.raulrezende.financialcontrol.entities.dto.CustomerDTO;
 import com.raulrezende.financialcontrol.repositories.CustomerRepository;
+import com.raulrezende.financialcontrol.services.exceptions.DatabaseException;
 import com.raulrezende.financialcontrol.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,6 +55,18 @@ public class CustomerService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new DatabaseException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 	
